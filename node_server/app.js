7dbe20f0e71d4aaf2c14fcfe659160a4e8f7101b
makino <makino@arte-inc.co.jp>
2013-05-28 19:29:14 +0900

    refactoring

--- node_server/app.js
+++ node_server/app.js
@@ -3,18 +3,28 @@ var express = require('express')
 var routes = require('./routes/index')
 var path = require('path')
 var app = express();
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.logger('dev'));
+});
+app.configure('development', function () {
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+});
 if('development' == app.get('env')) {
-    app.use(express.errorHandler());
 }
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
 app.get('/', routes.index);
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- node_server/app.ts
+++ node_server/app.ts
@@ -11,23 +11,31 @@ import routes = module('./routes/index')
 // import user = module('./routes/user')
 import path = module('path')
 
-var app = express();
+var app = <Express> express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+
+	app.use(express.logger('dev'));
+})
 
 // development only
-if ('development' == app.get('env')) {
+app.configure('development', function() {
+  app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+});
+
+// production only
+app.configure('production', function() {
   app.use(express.errorHandler());
-}
+});
 
 app.get('/', routes.index);
 // app.get('/users', user.list);
